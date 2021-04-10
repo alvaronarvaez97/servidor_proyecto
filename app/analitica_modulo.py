f2c345aa5bb6862ee69614cae87d4ed80ec669c2
LAPTOP-1KAV57JR\hp <alvaronarvaez97@gmail.com>
2021-04-10 09:24:35 -0500

    ajustes alertas

--- app/analitica_modulo.py
+++ app/analitica_modulo.py
@@ -36,24 +36,24 @@ class analitica():
         self.publicar("temperatura",msj_vetor[1])
         self.publicar("humedad",msj_vetor[3])
 
-        if msj_vetor[1] > str(31):
+        if msj_vetor[1] > str(22):
             self.publicar("alerta-temperatura","La temperatura esta por enciama del valor nominal")
 
-        if msj_vetor[1] < str(31):
-            if msj_vetor[1] > str(27):
+        if msj_vetor[1] < str(22):
+            if msj_vetor[1] > str(17):
                 self.publicar("alerta-temperatura","La temperatura estable")
 
-        if msj_vetor[1] < str(27):
+        if msj_vetor[1] < str(17):
             self.publicar("alerta-temperatura","La temperatura esta por debajo del valor nominal")
         
-        if msj_vetor[3] > str(85):
+        if msj_vetor[3] > str(70):
             self.publicar("alerta-humedad","La humedad esta por enciama del valor nominal")
 
-        if msj_vetor[3] < str(70):
+        if msj_vetor[3] < str(50):
             self.publicar("alerta-humedad","La humedad esta por debajo del valor nominal")
 
-        if msj_vetor[3] < str(85):
-            if msj_vetor[3] > str(70):
+        if msj_vetor[3] < str(70):
+            if msj_vetor[3] > str(50):
                 self.publicar("alerta-humedad","La humedad estable")
 
         self.analitica_descriptiva()
